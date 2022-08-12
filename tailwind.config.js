ebf904ff2583cf6abac6f77ee8ada1272a68592b
Tchilo <tchiloross@gmail.com>
2022-08-12 23:02:39 +0200

    Add assets and style desktop and mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      cyan: 'hsl(158, 36%, 37%)',
+      Cream: 'hsl(30, 38%, 92%)',
+      black: 'hsl(212, 21%, 14%)',
+      white: '#fff'
+    },
     extend: {},
   },
   plugins: [],
